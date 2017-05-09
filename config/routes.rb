f96b3be3bc447843ee60ea0c9f6e63ffa4bcc469
Lucas Sotty <lucas.sotty@wanadoo.fr>
2017-05-09 17:43:23 +0200

    Added login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
   resources :articles
   root to: "articles#index"
 end
